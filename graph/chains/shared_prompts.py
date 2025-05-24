43cf3e6fb8ef52577c2913362ef25aee80f795cc
Stuart McHattie <stuart.mchattie@gmail.com>
2025-05-24 22:51:36 +0100

    Update style guide

--- graph/chains/shared_prompts.py
+++ graph/chains/shared_prompts.py
@@ -1,11 +1,12 @@
 POST_STYLE = """The post should match the existing style of blogs already written for the site.\n
-    This includes the following style guidelines:\n
+    This includes the following style guidelines:\n\n
     - Keep the tone professional but not dry.\n
-    - The markdown should use one sentence per line.\n
+    - Use only UK spellings.\n
+    - Generate only one sentence per line of markdown.\n
     - Use level 2 and level 3 headers only to structure the post.\n
     - Place a horizontal rule before each level 2 heading.\n
     - Use hyphen bullet points for unordered lists.\n
-    - Ordered lists should use 1. on every item.\n
-    - Use code blocks for code snippets.\n
+    - Use "1." to number every item in an ordered list.\n
+    - Use code blocks for code snippets and surround them with blank lines.\n
     - The article should have an introduction without any header that provides conclusions to whet the appetite.\n
     - The final section of the post should be a level 2 header called "Wrapping Up" that summarizes the post and encourages further reading on the topic or to try out what has just been shown."""
