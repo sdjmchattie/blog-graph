e65cd6a6253345f9f84b445c442cf0310514c699
Stuart McHattie <stuart.mchattie@gmail.com>
2025-07-12 14:10:16 +0100

    Stop using mypy and python-dotenv
    python-dotenv is unnecessary with uv as long as you've set the
    UV_ENV_FILE environment variable to .env

--- lint.sh
+++ lint.sh
@@ -1,10 +1,7 @@
-echo "Running mypy..."
-poetry run mypy .
-
 echo
 echo "Running ruff checker..."
-poetry run ruff check
+uvx ruff check
 
 echo
 echo "Running ruff formatter..."
-poetry run ruff format
+uvx ruff format
--- main.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-
-load_dotenv()
-
-from graph import graph
-
-if __name__ == "__main__":
-    graph.invoke()
