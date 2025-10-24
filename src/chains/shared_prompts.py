0c7901d3eef6a0ce809e9e17afd18188a4fdf682
Stuart McHattie <stuart.mchattie@gmail.com>
2025-10-24 19:33:54 +0100

    Always use straight quotes

--- src/chains/shared_prompts.py
+++ src/chains/shared_prompts.py
@@ -5,6 +5,7 @@ POST_STYLE = """The post should match the existing style of blogs already writte
     - Generate only one sentence per line of markdown.\n
     - Use level 2 and level 3 headers only to structure the post.\n
     - Place a horizontal rule before each level 2 heading.\n
+    - Do not use curly or angled quote-marks. Only use straight quotes.\n
     - Use hyphen bullet points for unordered lists.\n
     - Use "1." to number every item in an ordered list.\n
     - Use code blocks for code snippets and surround them with blank lines.\n
